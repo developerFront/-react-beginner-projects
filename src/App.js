68eb85689b4124b2e42f30c73ded70b30274be5a
gstrud <gstrud@mail.ru>
2022-09-29 23:21:22 +0300

    photos

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.scss';
-
-function Collection({ name, images }) {
-  return (
-    <div className="collection">
-      <img className="collection__big" src={images[0]} alt="Item" />
-      <div className="collection__bottom">
-        <img className="collection__mini" src={images[1]} alt="Item" />
-        <img className="collection__mini" src={images[2]} alt="Item" />
-        <img className="collection__mini" src={images[3]} alt="Item" />
-      </div>
-      <h4>{name}</h4>
-    </div>
-  );
-}
+import { Collection } from './Collection';
 
 function App() {
+  const [collections, setCollection] = useState([]);
+
+  useEffect(() => {
+    fetch('https://6335efb08aa85b7c5d25c698.mockapi.io/photo_collection')
+      .then((res) => res.json())
+      .then((json) => {
+        setCollection(json);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert('Ошибка при получении данных');
+      });
+  }, []);
+
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Моя коллекция фотографий</h1>
-      <div className="top">
-        <ul className="tags">
-          <li className="active">Все</li>
+      <div className='top'>
+        <ul className='tags'>
+          <li className='active'>Все</li>
           <li>Горы</li>
           <li>Море</li>
           <li>Архитектура</li>
           <li>Города</li>
         </ul>
-        <input className="search-input" placeholder="Поиск по названию" />
+        <input className='search-input' placeholder='Поиск по названию' />
       </div>
-      <div className="content">
+      <div className='content'>
         <Collection
-          name="Путешествие по миру"
+          name='Путешествие по миру'
           images={[
             'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
             'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
@@ -40,9 +41,9 @@ function App() {
           ]}
         />
       </div>
-      <ul className="pagination">
+      <ul className='pagination'>
         <li>1</li>
-        <li className="active">2</li>
+        <li className='active'>2</li>
         <li>3</li>
       </ul>
     </div>
