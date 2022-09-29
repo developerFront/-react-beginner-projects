004ca8f62f240504659a0fb22592b7582be56fcb
gstrud <gstrud@mail.ru>
2022-09-29 23:52:02 +0300

    добавил коллекцию фото

--- src/App.js
+++ src/App.js
@@ -31,15 +31,9 @@ function App() {
         <input className='search-input' placeholder='Поиск по названию' />
       </div>
       <div className='content'>
-        <Collection
-          name='Путешествие по миру'
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
-        />
+        {collections.map((item) => (
+          <Collection name={item.name} images={item.photos} />
+        ))}
       </div>
       <ul className='pagination'>
         <li>1</li>
